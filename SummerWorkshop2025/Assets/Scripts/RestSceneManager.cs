4777df8b525261cd7f3ebd4fea7ad24ab299b6b2
GraceHauntedDoll <gracemayjagger@gmail.com>
2025-07-31 12:01:55 +0100

    Rest Site Shell implementation

---
+++ SummerWorkshop2025/Assets/Scripts/RestSceneManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestSceneManager : MonoBehaviour
+{
+    public GameObject RestScene;
+    public GameObject InventoryScene;
+
+    public GameObject FreeMoveScreen;
+    public GameObject RestAndInventoryScreen;
+
+    // this manager will be used to correctly display the inventory and rest 'scenes' when the appropriate buttons are pressed.
+    // this will be called when the player reaches the rest scene
+    // Should be ingored outside of rest scene
+
+
+    public int playerHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToInventoryScene()
+    {
+        RestScene.SetActive(false);
+        InventoryScene.SetActive(true);
+    }
+
+    public void MoveToRestScene()
+    {
+        InventoryScene.SetActive(false);
+        RestScene.SetActive(true);
+    }
+
+    public void LeaveRestScene()
+    {
+        InventoryScene.SetActive(false);
+        RestScene.SetActive(false);
+    }
+
+    public void SwitchToFreeMove()
+    {
+        RestAndInventoryScreen.SetActive(false);
+        FreeMoveScreen.SetActive(true);
+    }
+}
---
+++ SummerWorkshop2025/Assets/Scripts/ShellTypeSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Shell Type", menuName = "ScriptableObjects/ShellType")]
+public class ShellTypeSO : ScriptableObject
+{
+    public string shellName;
+    public Sprite shellImage;
+
+    public float moveSpeed;
+
+    public HealthStatsScriptableObject healthStats;
+    public AttackStatsScriptableObject[] playerAttacks;
+    public AttackStatsScriptableObject[] abilityAttacks;
+
+}
